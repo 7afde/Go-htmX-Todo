6555279f3ce2fa1f8d3d14cd59c6c1199851335a
hafed <hafed.zouyed@univ-constantine2.dz>
2024-08-07 06:54:40 +0100

    clean code

--- main.go
+++ main.go
@@ -5,35 +5,33 @@ import (
 	"net/http"
 	"text/template"
 )
-
 type Todo struct {
 	Id      int
 	Message string
 }
 
+var data = map[string][]Todo{
+	"Todos": {
+		Todo{Id: 1, Message: "Buy Milk"},
+	},
+}
+
+func todosHandler(w http.ResponseWriter, r *http.Request) {
+	templ := template.Must(template.ParseFiles("index.html"))
+
+	templ.Execute(w, data)
+}
+
+func addTodoHandler(w http.ResponseWriter, r *http.Request) {
+	message := r.FormValue("message")
+	templ := template.Must(template.ParseFiles("index.html"))
+	todo := Todo{Id: len(data["Todos"]) + 1, Message: message}
+	data["Todos"] = append(data["Todos"], todo)
+
+	templ.ExecuteTemplate(w, "todo-list-element", todo)
+}
+
 func main() {
-
-	data := map[string][]Todo{
-		"Todos": {
-			Todo{Id: 1, Message: "Buy Milk"},
-		},
-	}
-
-	todosHandler := func(w http.ResponseWriter, r *http.Request) {
-		templ := template.Must(template.ParseFiles("index.html"))
-
-		templ.Execute(w, data)
-	}
-
-	addTodoHandler := func(w http.ResponseWriter, r *http.Request) {
-		message := r.FormValue("message")
-		templ := template.Must(template.ParseFiles("index.html"))
-		todo := Todo{Id: len(data["Todos"]) + 1, Message: message}
-		data["Todos"] = append(data["Todos"], todo)
-
-		templ.ExecuteTemplate(w,"todo-list-element", todo)
-	}
-
 	http.HandleFunc("/", todosHandler)
 	http.HandleFunc("/add-todo", addTodoHandler)
 
